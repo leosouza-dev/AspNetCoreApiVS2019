76fda4aca5a2f533135d37a4b940e36dda1c9170
Leonardo Rodrigues de Souza <sarival@sarival.com.br>
2019-07-16 18:32:24 -0300

    Add project files.

---
+++ AspNetCoreApiVS2019/Migrations/20190716194330_inicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreApiVS2019.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    Documento = table.Column<string>(maxLength: 17, nullable: false),
+                    TipoFornecedor = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+        }
+    }
+}
---
+++ AspNetCoreApiVS2019/Models/Fornecedor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreApiVS2019.Models
+{
+    public class Fornecedor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} tem que ter entre {2} e {1} caracteres", MinimumLength =2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(17, ErrorMessage = "O campo {0} tem que ter entre {2} e {1} caracteres", MinimumLength = 13)]
+        public string Documento { get; set; }
+
+        public string TipoFornecedor { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
